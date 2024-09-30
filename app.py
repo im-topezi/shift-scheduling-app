7b988f42fe2177b1815bee28469bd56053495b06
Toivo Närhi <toivo.narhi@helsinki.fi>
2024-09-30 17:49:06 +0300

    Added a feature where if there is no admin user, the first person to log in will be made an admin. Also added a "tools" button to the index page, what is visible to admins only. Tools page is still under constrution.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 
@@ -8,36 +8,64 @@ from os import getenv
 
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key=getenv("SECRET_KEY")
 db = SQLAlchemy(app)
+
+def admin_check(username):
+    admin_search=db.session.execute(text("SELECT username FROM users WHERE role=(SELECT id FROM roles WHERE description='admin')"))
+    admins=[item[0] for item in admin_search.fetchall()]
+    return username in admins
+
+
 
 
 @app.route("/")
 def index():
-    #db.session.execute(text("INSERT INTO testi (data) VALUES ('testi testi')")) Just some testing of the database
-    #db.session.commit()
-    return render_template("index.html")
-
-
-
-@app.route("/homepage",methods=["POST"])
-def homepage():
     times=range(8,21)
+    days=["maanantai","tiistai","keskiviikko","torstai","perjantai"]
+    return render_template("index.html",times=times,days=days)
+
+@app.route("/tools",methods=["POST"])
+def tools():
+    return render_template("tools.html")
+
+
+@app.route("/set_user_role",methods=["POST"])
+def set_user_role():
+    
+
+@app.route("/login",methods=["POST"])
+def login():
+
     result=db.session.execute(text("SELECT username FROM users"))
-    usernames=result.fetchall()
+    usernames=[item[0] for item in result.fetchall()]
     username=request.form["username"]
+    session["username"]=username
     if username not in usernames:
         sql="INSERT INTO users (username) VALUES (:username)"
         db.session.execute(text(sql),{"username":username})
         db.session.commit()
-    return render_template("homepage.html",username=username,times=times)
+    role_search=db.session.execute(text("SELECT description FROM roles WHERE description='admin'"))
+    admin_role=role_search.fetchone()
+    if not admin_role:
+        db.session.execute(text("INSERT INTO roles (description) VALUES ('admin')"))
+        db.session.commit()
+        sql="UPDATE users SET role=(SELECT id FROM roles WHERE description='admin') WHERE username=(:username)"
+        db.session.execute(text(sql),{"username":username})
+        db.session.commit()
+    session["admin"]=False
+    if admin_check(username):
+        session["admin"]=True
+    return redirect("/")
 
 
 
 
-@app.route("/form")
+
+"""@app.route("/form")
 def form():
     return render_template("form.html")
 
 @app.route("/result", methods=["POST"])
 def result():
-    return render_template("result.html", name=request.form["name"])
+    return render_template("result.html", name=request.form["name"])"""
--- schema.sql
+++ schema.sql
@@ -11,19 +11,26 @@ CREATE TABLE "users" (
 
 CREATE TABLE "roles" (
   "id" SERIAL PRIMARY KEY,
-  "description" varchar
+  "description" varchar UNIQUE
 );
 
 CREATE TABLE "shifts" (
   "time_of_day" time,
-  "day_of_week" varchar,
+  "day_id" integer,
   "shift_type_id" integer,
   "shift_worker" integer,
-  PRIMARY KEY ("time_of_day", "day_of_week")
+  PRIMARY KEY ("time_of_day", "day_id")
 );
+CREATE TABLE "days" (
+ "id" INTEGER PRIMARY KEY,
+ "day_of_week" varchar
+);
+
 
 ALTER TABLE "shifts" ADD FOREIGN KEY ("shift_type_id") REFERENCES "shift_type" ("id");
 
 ALTER TABLE "shifts" ADD FOREIGN KEY ("shift_worker") REFERENCES "users" ("id");
 
 ALTER TABLE "users" ADD FOREIGN KEY ("role") REFERENCES "roles" ("id");
+
+ALTER TABLE "shifts" ADD FOREIGN KEY ("day_id") REFERENCES "days" ("id");
